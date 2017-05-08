4839356beb327e95ac222d4238ed04b391c3fd26
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-05-08 17:36:31 -0500

    slideActual listo

---
+++ src/subirPromo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+
+
+class SubirPromo extends Component {
+  render() {
+    return (
+      <div>
+      <h1>subir promo</h1>
+      </div>
+    );
+  }
+}
+
+export default SubirPromo;
